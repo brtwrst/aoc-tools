34d7cbb74d77980b75bf78addaeea12b987a7ac4
brtwrst <6873862+brtwrst@users.noreply.github.com>
2020-12-31 21:18:53 +0100

    fixes

---
+++ aocd/structs.py
@@ -0,0 +1,22 @@
+class Vec2():
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+
+    def __add__(self, other):
+        return Vec2(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other):
+        return Vec2(self.x - other.x, self.y - other.y)
+
+    def __repr__(self):
+        return f'Vec2({self.x}, {self.y})'
+
+    def __hash__(self):
+        return hash((self.x, self.y))
+
+    def __eq__(self, other):
+        return (self.x, self.y) == (other.x, other.y)
+
+    def __ne__(self, other):
+        return not(self == other)
