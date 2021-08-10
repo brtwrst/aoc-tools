056e409adb936a3178876a53d53888f4d36195a1
brtwrst <6873862+brtwrst@users.noreply.github.com>
2021-08-10 15:52:21 +0000

    update setup.py

--- setup.py
+++ setup.py
@@ -4,21 +4,21 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="aoc-tools-brtwrst", # Replace with your own username
+    name="aoc-tools-brtwrst",
     version="0.0.1",
     author="brtwrst",
     author_email="brtwrst@outlook.com",
     description="Advent of Code tools",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/brtwrst/aoc",
+    url="https://github.com/brtwrst/aoc-tools",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.8',
+    python_requires='>=3.9',
     install_requires=[
         'requests',
     ]
